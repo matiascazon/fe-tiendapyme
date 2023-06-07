a6c6bc0aa5919317a4625c56c392a601edc63cc5
DiegoCzn <diieego.cazon@gmail.com>
2023-06-06 23:26:02 -0300

    Footer

---
+++ src/Componentes/footer/Footer_col_2.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../../assets/css/plugins/swiper-bundle.min.css';
+import '../../assets/css/plugins/glightbox.min.css';
+import '../../assets/css/vendor/bootstrap.min.css';
+import '../../assets/css/style.css';
+
+function Footer_col_2() {
+  return (
+    <div class="col-lg-3 col-md-6">
+                        <div class="footer__widget">
+                            <h2 class="footer__widget--title ">Quick Links <button class="footer__widget--button" aria-label="footer widget button"></button>
+                                <svg class="footer__widget--title__arrowdown--icon" xmlns="http://www.w3.org/2000/svg" width="12.355" height="8.394" viewBox="0 0 10.355 6.394">
+                                    <path  d="M15.138,8.59l-3.961,3.952L7.217,8.59,6,9.807l5.178,5.178,5.178-5.178Z" transform="translate(-6 -8.59)" fill="currentColor"></path>
+                                </svg>
+                            </h2>
+                            <ul class="footer__widget--menu footer__widget--inner">
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="about.html">About Us</a></li>
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="wishlist.html">Wishlist</a></li>
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="contact.html">Contact Us</a></li>
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="privacy-policy.html">Privacy Policy</a></li>
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="faq.html">Frequently</a></li>
+                            </ul>
+                        </div>
+    </div>
+  )
+}
+
+export default Footer_col_2
---
+++ src/Componentes/footer/Footer_copy.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../assets/css/plugins/swiper-bundle.min.css';
+import '../../assets/css/plugins/glightbox.min.css';
+import '../../assets/css/vendor/bootstrap.min.css';
+import '../../assets/css/style.css';
+
+function Footer_copy() {
+  return (
+    <div class="footer__bottom d-flex justify-content-between align-items-center">
+                <p class="copyright__content">Copyright © 2022 <a class="copyright__content--link" href="index.html">Furea</a> . All Rights Reserved.Design By Furea</p>
+                <div class="footer__payment text-right">
+                    <img class="footer__payment--visa__card display-block" src="assets/img/other/payment-visa-card.webp" alt="visa-card"/>
+                </div>
+    </div>
+  )
+}
+
+export default Footer_copy
